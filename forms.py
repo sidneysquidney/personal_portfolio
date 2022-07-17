b05d5a3a9d85f5bfee4fcfa902581dc2787e9d73
sidneysquidney <sidneysquidney@hotmail.co.uk>
2022-07-17 10:01:44 +0100

    functionality of menu added. styling incomplete

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email, Length, ValidationError
 from wtforms.fields.html5 import EmailField  
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(max=50, min=5, message='Name must be between 5 and 50 characters')])
-    email = EmailField('Email', validators=[DataRequired(), Email('Please enter a valid email address'), Length(max=100)])
-    request = TextAreaField('Reason For Contact?', validators=[DataRequired(), Length(max=1000, message='Messagee must be under 1000 characters')])
-    submit = SubmitField('Submit')
+    name = StringField('Your Name', validators=[DataRequired(), Length(max=50, min=5, message='Name must be between 5 and 50 characters')])
+    email = EmailField('Your Email', validators=[DataRequired(), Email('Please enter a valid email address'), Length(max=100)])
+    request = TextAreaField('Your Message', validators=[DataRequired(), Length(max=1000, message='Messagee must be under 1000 characters')])
+    submit = SubmitField('SEND MESSAGE')
