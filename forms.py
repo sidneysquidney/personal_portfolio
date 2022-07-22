44b9bf8ced0a60accd11f6e305328b579beb8111
sidneysquidney <sidneysquidney@hotmail.co.uk>
2022-07-22 12:10:22 +0100

    main layout complete

--- forms.py
+++ forms.py
@@ -6,5 +6,5 @@ from wtforms.fields.html5 import EmailField
 class ContactForm(FlaskForm):
     name = StringField('Your Name', validators=[DataRequired(), Length(max=50, min=5, message='Name must be between 5 and 50 characters')])
     email = EmailField('Your Email', validators=[DataRequired(), Email('Please enter a valid email address'), Length(max=100)])
-    request = TextAreaField('Your Message', validators=[DataRequired(), Length(max=1000, message='Messagee must be under 1000 characters')])
+    request = TextAreaField('Your Message', validators=[DataRequired(), Length(max=1000, message='Messagee must be under 1000 characters')], render_kw={'class': 'form-control', 'rows': 5})
     submit = SubmitField('SEND MESSAGE')
